2125a847b4c883b1386f4248bf0176be85f66a47
Kiko Seijo <kiko@sunnyface.com>
2018-01-22 23:32:16 +0100

    Restructure.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './constants/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
